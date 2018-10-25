dba9b2b3889a02d8188e05bdc5f527d9443e9675
LeoAttn <leo.massonnat@hotmail.fr>
2018-10-25 21:04:00 +0200

    iframe

--- map.js
+++ map.js
@@ -4,8 +4,14 @@ class Map extends HTMLElement {
 
         const shadow = this.attachShadow({mode: 'open'});
 
-        const osm = document.createElement('div');
-        osm.id = "basicMap";
+        const osm = document.createElement('iframe');
+        osm.setAttribute('width', '100%');
+        osm.setAttribute('height', '400px');
+        osm.setAttribute('frameborder', 0);
+        osm.setAttribute('scrolling', 'no');
+        osm.setAttribute('marginheight', 0);
+        osm.setAttribute('marginwidth', 0);
+        osm.setAttribute('src', 'https://www.openstreetmap.org/export/embed.html?bbox=0.9448242187500001%2C48.45288728338137%2C3.2382202148437504%2C49.21670007971536&amp;layer=cyclemap" style="border: 1px solid black');
 
         const style = document.createElement('style');
         style.textContent = `
@@ -14,25 +20,8 @@ class Map extends HTMLElement {
         }
         `
 
-        const scriptMap = document.createElement('script');
-        scriptMap.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/openlayers/2.11/OpenLayers.js')
-        const script = document.createElement('script');
-        script.textContent = `
-        function init() {
-            map = new OpenLayers.Map("basicMap");
-            var fromProjection = new OpenLayers.Projection("EPSG:4326");
-            var toProjection = new OpenLayers.Projection("EPSG:900913");
-
-            map.addLayer(new OpenLayers.Layer.OSM());
-            map.setCenter(new OpenLayers.LonLat(2.30, 48.82).transform( fromProjection, toProjection), 10);
-        }
-        window.onload = init;
-        `
-
         shadow.appendChild(style);
         shadow.appendChild(osm);
-        shadow.appendChild(scriptMap);
-        shadow.appendChild(script);
     }
 }
 
